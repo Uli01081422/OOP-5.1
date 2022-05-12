ab2ed6a8ed2b4edc88b7e528a01b4496ce86a611
Uli01081422 <100164469+Uli01081422@users.noreply.github.com>
2022-05-12 21:13:45 +0300

    Add files via upload

---
+++ OOP 5.1A/OOP 5.1A/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../OOP 5.1A/Cursor.h"
+#include "../OOP 5.1A/Cursor.cpp"
+#include "../OOP 5.1A/Exception.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Cursor TEST(2, 2);
+			Assert::AreEqual(TEST.Check_2(2), true);
+		}
+	};
+}

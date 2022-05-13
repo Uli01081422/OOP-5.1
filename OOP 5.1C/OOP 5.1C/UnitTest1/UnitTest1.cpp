1d3d8a5fecbae026acf31a9c632bb6c3c0163d01
Uli01081422 <100164469+Uli01081422@users.noreply.github.com>
2022-05-13 09:39:09 +0300

    Add files via upload

---
+++ OOP 5.1C/OOP 5.1C/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../OOP 5.1C/FuzzyNumber_Private.h"
+#include "../OOP 5.1C/FuzzyNumber_Private.cpp"
+#include "../OOP 5.1C/FuzzyNumber.cpp"
+#include "../OOP 5.1C/FuzzyNumber.h"
+#include "../OOP 5.1C/Exception.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			FuzzyNumber_Private A(1, 1, 1);
+			A.SetX(5);
+			Assert::AreEqual(A.GetX(), 5.);
+
+		}
+	};
+}
